97a8e72d0fdb60b0dbcd0593571fe82827308e0c
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-12 12:31:01 +0300

    Fetch devto followers

--- pages/api/devto.js
+++ pages/api/devto.js
@@ -6,9 +6,21 @@ export default async (_, res) => {
     },
   });
 
+  const followersResponse = await fetch(
+    "https://dev.to/api/followers/users?per_page=300",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "api-key": process.env.DEVTO_API_KEY,
+      },
+    }
+  );
+
   const articles = await articlesResponse.json();
+  const followers = await followersResponse.json();
 
   return res.status(200).json({
     articles,
+    followersCount: followers.length,
   });
 };
