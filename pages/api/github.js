59e67670ad2deb29f98757fe75c6d557204dc87b
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-13 10:21:16 +0300

    Fetch github repos api

--- pages/api/github.js
+++ pages/api/github.js
@@ -1,15 +1,23 @@
 export default async (_, res) => {
-    const userResponse = await fetch("https://api.github.com/user", {
-      headers: {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": `token ${process.env.GITHUB_PERSONAL_TOKEN}`,
-      },
-    });
-  
-    const user = await userResponse.json();
-  
-    return res.status(200).json({
-      user,
-    });
-  };
-  
+  const userResponse = await fetch("https://api.github.com/user", {
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      Authorization: `token ${process.env.GITHUB_PERSONAL_TOKEN}`,
+    },
+  });
+
+  const reposResponse = await fetch("https://api.github.com/user/repos", {
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      Authorization: `token ${process.env.GITHUB_PERSONAL_TOKEN}`,
+    },
+  });
+
+  const user = await userResponse.json();
+  const repos = await reposResponse.json();
+
+  return res.status(200).json({
+    user,
+    repos,
+  });
+};
