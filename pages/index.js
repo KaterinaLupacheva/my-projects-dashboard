480c9dbf3b6a88b584055dc1a9e41999441c5ec3
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-16 16:24:40 +0300

    Start page clean up

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Button from "@material-ui/core/Button";
 import Ramonak from "./ramonak";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Ramonak />
