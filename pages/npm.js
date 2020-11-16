bcb1d8ad57efdac9960e4b82acc5a65dd2ac0b1f
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-16 16:39:01 +0300

    Npm page styles

--- pages/npm.js
+++ pages/npm.js
@@ -1,13 +1,17 @@
 import { packages } from "../src/constants/npm-packages";
 import NpmPackageCard from "../src/components/NpmPackageCard/npm-package-card.component";
+import { Typography, Box } from "@material-ui/core";
 
 const Npm = () => {
   return (
-    <div>
+    <Box style={{ width: "100%" }}>
+      <Typography variant="h3" align="center" gutterBottom>
+        NPM packages
+      </Typography>
       {packages.map((p, id) => (
         <NpmPackageCard packageName={p} key={id} />
       ))}
-    </div>
+    </Box>
   );
 };
 
--- src/components/NpmPackageCard/npm-package-card.component.js
+++ src/components/NpmPackageCard/npm-package-card.component.js
@@ -1,6 +1,9 @@
 import useSWR from "swr";
 import { fetcher } from "../../../utils/fetcher";
 import BackDropWithSpinner from "../BackDropWithSpinner/backdrop-with-spinner.component";
+import Paper from "@ramonak/paper";
+import "@ramonak/paper/dist/index.css";
+import { Typography } from "@material-ui/core";
 
 const NpmPackageCard = (props) => {
   const { packageName } = props;
@@ -10,7 +13,13 @@ const NpmPackageCard = (props) => {
   if (!data) return <BackDropWithSpinner open={true} />;
   if (!data.downloads) return <div>Error</div>;
 
-  return <>{data.downloads.package}</>;
+  return (
+    <Paper elevation={3}>
+      <Typography variant="h6" align="center">
+        {data.downloads.package}
+      </Typography>
+    </Paper>
+  );
 };
 
 export default NpmPackageCard;
