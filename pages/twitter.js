eef7343c67df73634215f88fec81e5f2377de28b
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-19 16:01:23 +0300

    Sort tweets added

--- pages/twitter.js
+++ pages/twitter.js
@@ -1,17 +1,59 @@
+import { useState } from "react";
 import CustomHead from "../src/components/Head/head";
 import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
 import BackDropWithSpinner from "../src/components/BackDropWithSpinner/backdrop-with-spinner.component";
 import TwitterUserInfo from "../src/components/TwitterUserInfo/twitter-user-info.component";
 import TweetCard from "../src/components/TweetCard/tweet-card.component";
-import { Box, Typography, Grid, Divider } from "@material-ui/core";
+import {
+  Box,
+  Typography,
+  Grid,
+  Divider,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+} from "@material-ui/core";
 
 const Twitter = () => {
   const { data, error } = useSWR("/api/twitter", fetcher);
+  const [sortType, setSortType] = useState("date");
 
   if (error) return <div>failed to load</div>;
   if (!data) return <BackDropWithSpinner open={true} />;
   if (!data.userData || !data.tweetsData) return <div>Error</div>;
+
+  const sortArray = (type) => {
+    const types = {
+      date: "created_at",
+      retweets: "retweet_count",
+      likes: "favorite_count",
+    };
+    const sortProperty = types[type];
+    const tweetsArr = data.tweetsData;
+    if (type === "date") {
+      return [...tweetsArr].sort(
+        (a, b) => Date.parse(b[sortProperty]) - Date.parse(a[sortProperty])
+      );
+    } else {
+      return [...tweetsArr].sort((a, b) => b[sortProperty] - a[sortProperty]);
+    }
+  };
+
+  const renderTweets = () => {
+    const sorted = sortArray(sortType);
+    return (
+      <Grid container spacing={2}>
+        {sorted.map((tweet, id) => (
+          <Grid item sm={6} key={id}>
+            <TweetCard {...tweet} />
+          </Grid>
+        ))}
+      </Grid>
+    );
+  };
 
   return (
     <>
@@ -32,13 +74,29 @@ const Twitter = () => {
             Tweets
           </Typography>
         </Box>
-        <Grid container spacing={2}>
-          {data.tweetsData.map((tweet, id) => (
-            <Grid item sm={6} key={id}>
-              <TweetCard {...tweet} />
-            </Grid>
-          ))}
-        </Grid>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Sorted by</FormLabel>
+          <RadioGroup
+            row
+            aria-label="sort"
+            name="sorted-articles"
+            value={sortType}
+            onChange={(event) => setSortType(event.target.value)}
+          >
+            <FormControlLabel
+              value="date"
+              control={<Radio />}
+              label="Published Date"
+            />
+            <FormControlLabel
+              value="retweets"
+              control={<Radio />}
+              label="Retweets"
+            />
+            <FormControlLabel value="likes" control={<Radio />} label="Likes" />
+          </RadioGroup>
+        </FormControl>
+        {renderTweets()}
       </Box>
     </>
   );
