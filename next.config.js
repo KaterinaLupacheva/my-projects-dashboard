eb02e125f621d164a8c30ea4585575782b5ab884
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-12-07 19:02:10 +0300

    Headers updated

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://katerinalupacheva.github.io" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
