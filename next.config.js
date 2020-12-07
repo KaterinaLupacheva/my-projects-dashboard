af194bf6c5f0be1bd1f44e2468b9ea4298fe227a
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-12-07 18:40:24 +0300

    Headers added to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,30 @@
 module.exports = {
   images: {
-    domains: ["res.cloudinary.com", "avatars2.githubusercontent.com", "pbs.twimg.com"],
+    domains: [
+      "res.cloudinary.com",
+      "avatars2.githubusercontent.com",
+      "pbs.twimg.com",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
