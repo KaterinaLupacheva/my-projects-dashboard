7a74dd40e3c045f85012d1f0515899e4033148aa
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-12-14 10:34:52 +0300

    Slugs updated and sorted by groups

--- pages/ramonak.js
+++ pages/ramonak.js
@@ -5,7 +5,8 @@ import BackDropWithSpinner from "../src/components/BackDropWithSpinner/backdrop-
 import StatCard from "../src/components/StatCard/stat-card.component";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import { SLUGS } from "../src/constants/slugs";
+import { SLUGS, RAMONAK_BLOG, NPM_PACKAGE } from "../src/constants/slugs";
+import ViewsTable from "../src/components/ViewsTable/views-table.component";
 
 const Ramonak = () => {
   const { data, error } = useSWR("/api/views", fetcher);
@@ -15,31 +16,46 @@ const Ramonak = () => {
   if (!data.docs) return <div>Error</div>;
 
   const mapSlugs = () => {
+    let id = 0;
     let mappedDocs = [];
+    const grouped = groupBy(SLUGS, "group");
+    console.log(grouped);
     SLUGS.forEach((obj) => {
       const doc = data.docs.find((d) => d.slug === obj.slug);
-      mappedDocs.push({ ...doc, description: obj.description });
+      if (doc) {
+        mappedDocs.push({
+          ...doc,
+          description: obj.description,
+          group: obj.group,
+        });
+      } else {
+        mappedDocs.push({ ...obj, _id: id, totalViews: 0 });
+        id++;
+      }
     });
     return mappedDocs;
   };
 
+  const groupBy = (arr, property) => {
+    return arr.reduce((acc, cur) => {
+      acc[cur[property]] = [...(acc[cur[property]] || []), cur];
+      return acc;
+    }, {});
+  };
+
   const docs = mapSlugs();
+  // console.log(docs)
 
   return (
     <>
       <CustomHead title="Projects stats" />
       <Box width="100%">
         <Typography variant="h2" align="center" gutterBottom>
-          Views count
+          |Blog posts Views count
         </Typography>
       </Box>
-      {docs.map((d) => (
-        <StatCard
-          key={d._id}
-          value={d.totalViews ? d.totalViews : 0}
-          title={d.description}
-        />
-      ))}
+
+      <ViewsTable data={docs} />
     </>
   );
 };
--- src/constants/slugs.js
+++ src/constants/slugs.js
@@ -8,128 +8,128 @@ export const SLUGS = [
     group: NPM_PACKAGE,
   },
   {
-    slug: "top-10-women-in-tech-business",
+    slug: "ramonak: top-10-women-in-tech-business",
     description: "Top10 women in tech business",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "what-is-progressive-web-app",
+    slug: "ramonak: what-is-progressive-web-app",
     description: "What is progressive web app",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "how-to-deploy-vaadin-14-app-to-heroku",
+    slug: "ramonak: how-to-deploy-vaadin-14-app-to-heroku",
     description: "How to deploy Vaading app to Heroku",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "top-5-benefits-of-having-a-website",
+    slug: "ramonak: top-5-benefits-of-having-a-website",
     description: "Top5 benefits of having a website",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "springboot–vaadin-keycloak–springsecurity-integration",
+    slug: "ramonak: springboot–vaadin-keycloak–springsecurity-integration",
     description:
       "Spring Boot Vaadin Keycloak Spring Security integration - part I",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "setting-up-vaadin-spring-boot-frontend-app",
+    slug: "ramonak: setting-up-vaadin-spring-boot-frontend-app",
     description: "Setting up Vaadin Spring Boot frontend app - part II",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "keycloak-server-installation-and-realm-creation",
+    slug: "ramonak: keycloak-server-installation-and-realm-creation",
     description: "Keycloak server instalation and realm creation - part III",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "keycloak-adding-roles-and-users",
+    slug: "ramonak: keycloak-adding-roles-and-users",
     description: "Keycloak: adding roles and users  - part IV",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "keycloak-clients-configuration",
+    slug: "ramonak: keycloak-clients-configuration",
     description: "Keycloak: clients configuration  - part V",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "spring-boot–keycloak-spring-security-integration",
+    slug: "ramonak: spring-boot–keycloak-spring-security-integration",
     description: "Spring boot Keycloak Spring Security integration  - part VI",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "vaadin–keycloak–spring-security-integration",
+    slug: "ramonak: vaadin–keycloak–spring-security-integration",
     description: "Vaadin Keycloak Spring Security integration  - part VII",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "vaadin-drag-and-drop",
+    slug: "ramonak: vaadin-drag-and-drop",
     description: "Vaadin drag and drop",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "tabata-fitness-app-first-project",
+    slug: "ramonak: tabata-fitness-app-first-project",
     description: "Tabata: first project",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "react-context-api-update-from-nested-component",
+    slug: "ramonak: react-context-api-update-from-nested-component",
     description: "React: context API - update from nested component",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "javascript-how-to-merge-multiple-objects",
+    slug: "ramonak: javascript-how-to-merge-multiple-objects",
     description: "Javascript: how to merge multiple objects",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "react-how-to-sort-array-of-objects-with-dropdown-and-hooks",
+    slug: "ramonak: react-how-to-sort-array-of-objects-with-dropdown-and-hooks",
     description: "React: how to sort array of objects with dropdown and hooks",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "vaadin-upload-file-to-amazon-s3-java",
+    slug: "ramonak: vaadin-upload-file-to-amazon-s3-java",
     description: "Vaadin: upload file to Amazon S3",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "react-progress-bar",
+    slug: "ramonak: react-progress-bar",
     description:
       "React: How to create a custom progress bar component in 5 minutes",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "react-publish-to-npm",
+    slug: "ramonak: react-publish-to-npm",
     description:
       "How to publish a custom React component to NPM using Create React Library",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "react-google-analytics-dashboard",
+    slug: "ramonak: react-google-analytics-dashboard",
     description:
       "How to develop a custom Google Analytics Dashboard using Google Analytics Reporting API v4 and React.js",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "react-excel-render-update-api",
+    slug: "ramonak: react-excel-render-update",
     description:
       "Render, update, and transform Excel spreadsheet data into an array of objects using React.js",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "javascript-return-promise",
+    slug: "ramonak: javascript-return-promise",
     description:
       "Javascript: How to access the return value of a Promise object",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "react-custom-hooks",
+    slug: "ramonak: react-custom-hooks",
     description: "How to write and use custom hooks in React.js",
     group: RAMONAK_BLOG,
   },
   {
-    slug: "react-button-group",
+    slug: "ramonak: react-button-group",
     description:
       "React: How to create a custom Button Group component in 5 minutes",
     group: RAMONAK_BLOG,
