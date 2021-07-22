84b3ddc6b08fd6f8e4726a4eb947332c51ad3c1e
katerinalupacheva <katerinalupacheva@gmail.com>
2021-07-22 22:28:49 +0300

    APP_API_KEY added

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -6,61 +6,67 @@ import { connectToDatabase } from '../../utils/mongodb';
 import { queryDevto } from './devto';
 
 export default async (
-  _: NextApiRequest,
+  req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
   try {
-    const { followersCount } = await queryDevto();
+    const { authorization } = req.headers;
 
-    const { db } = await connectToDatabase();
-    const statsCollection = await db.collection('stats');
+    if (authorization === `Bearer ${process.env.APP_API_KEY}`) {
+      const { followersCount } = await queryDevto();
 
-    // Check and see if the doc exists.
-    let existedDoc = await statsCollection
-      .find({
-        name: 'devto',
-      })
-      .toArray()
-      .then((docs: DevtoStatsDoc[]) => {
-        return docs[0];
-      });
+      const { db } = await connectToDatabase();
+      const statsCollection = await db.collection('stats');
 
-    //if doesn't exist, create new doc
-    if (!existedDoc) {
-      await statsCollection
-        .insertOne({
+      // Check and see if the doc exists.
+      let existedDoc = await statsCollection
+        .find({
           name: 'devto',
-          followers: [
-            { date: moment().format('DD-MM-YYYY'), count: followersCount },
-          ],
         })
-        /* eslint-disable  @typescript-eslint/no-explicit-any */
-        .then((doc: any) => {
-          existedDoc = doc.ops[0];
+        .toArray()
+        .then((docs: DevtoStatsDoc[]) => {
+          return docs[0];
         });
-    }
 
-    //check if api was called today
-    const dateExists = existedDoc.followers.find(
-      (item: FollowersData) => item.date === moment().format('DD-MM-YYYY')
-    );
+      //if doesn't exist, create new doc
+      if (!existedDoc) {
+        await statsCollection
+          .insertOne({
+            name: 'devto',
+            followers: [
+              { date: moment().format('DD-MM-YYYY'), count: followersCount },
+            ],
+          })
+          /* eslint-disable  @typescript-eslint/no-explicit-any */
+          .then((doc: any) => {
+            existedDoc = doc.ops[0];
+          });
+      }
 
-    //update followers count
-    if (!dateExists) {
-      await statsCollection.updateOne(
-        { name: 'devto' },
-        {
-          $push: {
-            followers: {
-              date: moment().format('DD-MM-YYYY'),
-              count: followersCount,
-            },
-          },
-        }
+      //check if api was called today
+      const dateExists = existedDoc.followers.find(
+        (item: FollowersData) => item.date === moment().format('DD-MM-YYYY')
       );
-    }
 
-    res.status(200).json('status: OK');
+      //update followers count
+      if (!dateExists) {
+        await statsCollection.updateOne(
+          { name: 'devto' },
+          {
+            $push: {
+              followers: {
+                date: moment().format('DD-MM-YYYY'),
+                count: followersCount,
+              },
+            },
+          }
+        );
+      }
+
+      res.status(200).json('status: OK');
+    } else {
+      res.status(401).json('Not authorized');
+    }
   } catch (error) {
     res.status(500).json({ statusCode: 500, message: error.message });
   }
