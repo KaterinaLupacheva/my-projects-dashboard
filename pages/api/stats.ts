82448c1b7bc060980428480db3dc63cd171f3b3d
katerinalupacheva <katerinalupacheva@gmail.com>
2021-07-23 15:11:38 +0300

    Add new devto followers stats - use POST method

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -9,65 +9,78 @@ export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  try {
-    const { authorization } = req.headers;
+  if (req.method === 'POST') {
+    try {
+      const { authorization } = req.headers;
 
-    if (authorization === `Bearer ${process.env.APP_API_KEY}`) {
-      const { followersCount } = await queryDevto();
+      if (authorization === `Bearer ${process.env.APP_API_KEY}`) {
+        const { followersCount } = await queryDevto();
 
-      const { db } = await connectToDatabase();
-      const statsCollection = await db.collection('stats');
+        const { db } = await connectToDatabase();
+        const statsCollection = await db.collection('stats');
 
-      // Check and see if the doc exists.
-      let existedDoc = await statsCollection
-        .find({
-          name: 'devto',
-        })
-        .toArray()
-        .then((docs: DevtoStatsDoc[]) => {
-          return docs[0];
-        });
-
-      //if doesn't exist, create new doc
-      if (!existedDoc) {
-        await statsCollection
-          .insertOne({
+        // Check and see if the doc exists.
+        let existedDoc = await statsCollection
+          .find({
             name: 'devto',
-            followers: [
-              { date: moment().format('DD-MM-YYYY'), count: followersCount },
-            ],
           })
-          /* eslint-disable  @typescript-eslint/no-explicit-any */
-          .then((doc: any) => {
-            existedDoc = doc.ops[0];
+          .toArray()
+          .then((docs: DevtoStatsDoc[]) => {
+            return docs[0];
           });
-      }
 
-      //check if api was called today
-      const dateExists = existedDoc.followers.find(
-        (item: FollowersData) => item.date === moment().format('DD-MM-YYYY')
-      );
+        //if doesn't exist, create new doc
+        if (!existedDoc) {
+          await statsCollection
+            .insertOne({
+              name: 'devto',
+              followers: [
+                { date: moment().format('DD-MM-YYYY'), count: followersCount },
+              ],
+            })
+            /* eslint-disable  @typescript-eslint/no-explicit-any */
+            .then((doc: any) => {
+              existedDoc = doc.ops[0];
+            });
+        }
 
-      //update followers count
-      if (!dateExists) {
-        await statsCollection.updateOne(
-          { name: 'devto' },
-          {
-            $push: {
-              followers: {
-                date: moment().format('DD-MM-YYYY'),
-                count: followersCount,
-              },
-            },
-          }
+        //check if api was called today
+        const dateExists = existedDoc.followers.find(
+          (item: FollowersData) => item.date === moment().format('DD-MM-YYYY')
         );
-      }
 
-      res.status(200).json('status: OK');
-    } else {
-      res.status(401).json('Not authorized');
+        //update followers count
+        if (!dateExists) {
+          await statsCollection.updateOne(
+            { name: 'devto' },
+            {
+              $push: {
+                followers: {
+                  date: moment().format('DD-MM-YYYY'),
+                  count: followersCount,
+                },
+              },
+            }
+          );
+        }
+
+        res.status(200).json('status: OK');
+      } else {
+        res.status(401).json('Not authorized');
+      }
+    } catch (error) {
+      res.status(500).json({ statusCode: 500, message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ statusCode: 500, message: error.message });
+  }
+
+  if (req.method === 'GET') {
+    const { db } = await connectToDatabase();
+    const statsCollection = await db.collection('stats');
+
+    const docs = await statsCollection.find({ name: 'devto' }).toArray();
+
+    return res.status(200).json({
+      followersStats: docs[0].followers,
+    });
   }
 };
