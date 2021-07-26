7556c50a316e34801eb2c32f3a3a9204b437ff3d
katerinalupacheva <katerinalupacheva@gmail.com>
2021-07-26 22:16:45 +0300

    Npm page - loading spinners

--- components/NpmPackageCard.tsx
+++ components/NpmPackageCard.tsx
@@ -1,17 +1,22 @@
-import { Grid, Typography } from '@material-ui/core';
+import { CircularProgress, Grid, Typography } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@ramonak/paper';
 import useSWR from 'swr';
 
 import { INpmDownloads } from '../types/general';
 import { fetcher } from '../utils/fetcher';
-import BackDropWithSpinner from './BackdropWithSpinner';
 import CustomLineChart from './LineChart';
 import StatCard from './StatCard';
 
 const useStyles = makeStyles((theme: Theme) => ({
   paperContainer: {
     padding: theme.spacing(1),
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 50,
   },
 }));
 
@@ -20,12 +25,10 @@ interface NpmPackageCardProps {
 }
 
 const NpmPackageCard = ({ packageName }: NpmPackageCardProps): JSX.Element => {
-  const { data, error } = useSWR(`/api/npm/${packageName}`, fetcher);
   const classes = useStyles();
+  const { data, error } = useSWR(`/api/npm/${packageName}`, fetcher);
 
   if (error) return <div>failed to load</div>;
-  if (!data) return <BackDropWithSpinner open={true} />;
-  if (!data.lastMonthDownloads) return <div>Error</div>;
 
   const averageDownloads = (downloadsData: INpmDownloads[]) => {
     const sum = downloadsData.reduce((a, b) => a + b.downloads, 0);
@@ -35,34 +38,42 @@ const NpmPackageCard = ({ packageName }: NpmPackageCardProps): JSX.Element => {
 
   return (
     <Paper elevation={3} customClass={classes.paperContainer}>
-      <Typography variant="h6" align="center" gutterBottom>
-        {data.lastMonthDownloads.package}
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={9}>
-          <CustomLineChart
-            data={data.lastMonthDownloads.downloads}
-            lineDataKey="downloads"
-            xDataKey="day"
-          />
-        </Grid>
-        <Grid item xs={12} md={3}>
-          <Grid container>
-            <Grid item xs={12} sm={6} md={12}>
-              <StatCard
-                title="Average per day"
-                value={averageDownloads(data.lastMonthDownloads.downloads)}
+      {!data ? (
+        <div className={classes.container}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.lastMonthDownloads.package}
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={9}>
+              <CustomLineChart
+                data={data?.lastMonthDownloads.downloads}
+                lineDataKey="downloads"
+                xDataKey="day"
               />
             </Grid>
-            <Grid item xs={12} sm={6} md={12}>
-              <StatCard
-                title="Weekly downloads"
-                value={data.weeklyDownloads.downloads}
-              />
+            <Grid item xs={12} md={3}>
+              <Grid container>
+                <Grid item xs={12} sm={6} md={12}>
+                  <StatCard
+                    title="Average per day"
+                    value={averageDownloads(data?.lastMonthDownloads.downloads)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} md={12}>
+                  <StatCard
+                    title="Weekly downloads"
+                    value={data?.weeklyDownloads.downloads}
+                  />
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </Grid>
+        </>
+      )}
     </Paper>
   );
 };
