53b5781fdc20f200cd57893ce5441095c249bb13
katerinalupacheva <katerinalupacheva@gmail.com>
2021-07-28 13:28:49 +0300

    Articles context added

---
+++ context/ArticlesContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useContext } from 'react';
+import { IArticle } from '../types/general';
+
+type ArticlesState = {
+  articles: IArticle[];
+  setArticles: (data: IArticle[]) => void;
+};
+
+type ArticlesContextProviderProps = {
+  children: React.ReactNode;
+};
+
+const ArticlesContext = createContext<ArticlesState | undefined>(undefined);
+
+const ArticlesContextProvider = ({
+  children,
+}: ArticlesContextProviderProps): JSX.Element => {
+  const [articles, setArticles] = useState<IArticle[]>([]);
+
+  return (
+    <ArticlesContext.Provider value={{ articles, setArticles }}>
+      {children}
+    </ArticlesContext.Provider>
+  );
+};
+
+const useArticlesContext = (): ArticlesState => {
+  const context = useContext(ArticlesContext);
+  if (context === undefined) {
+    throw new Error(
+      'useArticlesContext must be used within a ArticlesContextProvider'
+    );
+  }
+  return context;
+};
+
+export { ArticlesContextProvider, useArticlesContext };
